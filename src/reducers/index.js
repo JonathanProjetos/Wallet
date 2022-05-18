12087432cb7e3aa0c2e1c6f24b06f0a71402423a
Jonathan Santos <jonathan01347@gmail.com>
2022-05-18 18:52:31 -0300

    ajustes no lint

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import emailReducer from './user';
+import walletReducer from './wallet';
 // import wallet from './wallet';
 
 // Configure os seus reducers.
 const rooReducer = combineReducers({
   user: emailReducer,
+  wallet: walletReducer,
 });
 
 export default rooReducer;
